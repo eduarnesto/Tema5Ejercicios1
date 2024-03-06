456cd316a688227a391d893671153796842c1b74
eduarnesto <eduardo.arnesto@iesnervion.es>
2024-03-06 09:16:35 +0100

    Ejercicio 4

--- src/ejercicio04/Articulo.java
+++ src/ejercicio04/Articulo.java
@@ -3,22 +3,84 @@ package ejercicio04;
 public class Articulo {
 	private String nombre = "";
 	private float precio;
-	private final int IVA = 21;
+	public final int IVA = 21;
 	private int cuantosQuedan;
-	
+
 	public Articulo(String nombre, float precio, int cuantosQuedan) {
 		super();
+		setNombre(nombre);
+		setPrecio(precio);
+		setCuantosQuedan(cuantosQuedan);
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
 		if (nombre != null && !nombre.equals(""))
 			this.nombre = nombre;
+	}
+
+	public float getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(float precio) {
 		if (precio > 0)
 			this.precio = precio;
-		if (cuantosQuedan>=0)
+	}
+
+	public int getCuantosQuedan() {
+		return cuantosQuedan;
+	}
+
+	public void setCuantosQuedan(int cuantosQuedan) {
+		if (cuantosQuedan >= 0)
 			this.cuantosQuedan = cuantosQuedan;
 	}
 	
-	public void imprime() {
-		System.out.println("");
+	@Override
+	public String toString() {
+		String info = "";
+
+		info = "Articulo: " + this.nombre + "\n" + "Precio sin IVA: " + this.precio + "\n" + "Disponibles: "
+				+ this.cuantosQuedan;
+
+		return info;
 	}
-	
-	
+
+	public float getPVP() {
+		float precio;
+
+		precio = this.precio * (1 + (float) this.IVA / 100);
+
+		return precio;
+	}
+
+	public float getPVPDescuento(int descuento) {
+		float precio;
+
+		precio = this.precio * (1 + (float) this.IVA / 100) * (1 - (float) descuento / 100);
+
+		return precio;
+	}
+
+	public boolean vender() {
+		boolean vendido = false;
+
+		if (this.cuantosQuedan > 0) {
+			this.cuantosQuedan--;
+			vendido = true;
+		}
+
+		return vendido;
+	}
+
+	public void almacenar(int cantidad) {
+
+		this.cuantosQuedan += cantidad;
+
+	}
+
 }
--- src/ejercicio04/Main.java
+++ src/ejercicio04/Main.java
@@ -3,7 +3,29 @@ package ejercicio04;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Creo difrentes productos
+		Articulo art1 = new Articulo ("Campana", 100, 5);
+		Articulo art2 = new Articulo ("Manzana", 100, 5);
+		Articulo art3 = new Articulo ("Tila", 100, 5);
+		
+		//Pruebo la funcion para que devuleva el precio
+		System.out.println(art1.getPVP());
+		
+		//Pruebo la funcion para que devuelvo el precio con descuento
+		System.out.println(art2.getPVPDescuento(20));
+		
+		//Vendo un producto del articulo 3
+		if (art3.vender()) {
+			System.out.println("Has comprado " + art3.getNombre() + " exitosamente");
+		} else {
+			System.out.println("No hay articulos disponibles de " + art3.getNombre());
+		}
+		
+		//Repongo articulo 3
+		System.out.println("Teniamos " + art3.getCuantosQuedan() + " unidades de " + art3.getNombre());
+		System.out.println("Repongo 5 articulos");
+		art3.almacenar(5);
+		System.out.println("Ahora hay " + art3.getCuantosQuedan() + " unidades de " + art3.getNombre());
 
 	}
 
